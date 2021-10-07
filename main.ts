d893fe366f0e450abae7fa7a22fa19dfcbd613b7
Thor Correia <thor.lucas@berkeley.edu>
2021-10-07 02:32:48 -0700

    naive implementation of log

--- main.ts
+++ main.ts
@@ -1,9 +1,32 @@
-import { Plugin } from 'obsidian';
+import { moment, Plugin, TFile } from 'obsidian';
+import { getAllDailyNotes, getDailyNote, createDailyNote } from 'obsidian-daily-notes-interface';
 
 export default class DailyNoteUtils extends Plugin {
-	async onload() {
-	}
+	api = {
+		getDailyNote: (): TFile => {
+			const now = moment();
+			const allDailyNotes = getAllDailyNotes();
+			const note = getDailyNote(now, allDailyNotes);
+			return note;
+		},
 
-	onunload() {
+		logToDailyNote: (text: string): void => {
+			const note = this.api.getDailyNote();
+			const now = moment();
+			this.app.vault.adapter.read(note.path)
+			.then((noteData) => {
+				const dateStr = now.format('HH:MM');
+				noteData += `- **${dateStr}**: ${text}\n`;
+				return this.app.vault.adapter.write(note.path, noteData);
+			})
+			.then((ret) => {
+				console.log(ret);
+			});
+		}
+	};
+
+	async onload() {
+		// @ts-ignore
+		window.dailyNoteUtils = this.api;
 	}
 }
